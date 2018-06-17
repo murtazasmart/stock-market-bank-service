1bf874bdcba387343634b93b020a542eb97962a8
lalendra94 <kasun.silva94@yahoo.com>
2018-06-17 14:37:38 +0530

    update

--- application/controllers/Bank.php
+++ application/controllers/Bank.php
@@ -16,6 +16,9 @@ class Bank extends REST_Controller {
         $this->load->model("AccountModel");
         $this->load->model("TransactionModel");
     }
+    public function index(){
+        echo "<h2>Bank API - UCD CS batch 7 final project_ team exit - https://github.com/murtazasmart/stock-market-bank-service</h2>";
+    }
 
     public function account_get($id = NULL) {
 //        $id = $this->get('id');
