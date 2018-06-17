4dc18125a94f19101d63ae469988de800201ef22
lalendra94 <kasun.silva94@yahoo.com>
2018-06-17 14:41:24 +0530

    Updates

--- application/controllers/Bank.php
+++ application/controllers/Bank.php
@@ -15,7 +15,7 @@ class Bank extends REST_Controller {
         parent::__construct();
         $this->load->model("AccountModel");
         $this->load->model("TransactionModel");
-    }
+    } 
     public function index(){
         echo "<h2>Bank API - UCD CS batch 7 final project_ team exit - https://github.com/murtazasmart/stock-market-bank-service</h2>";
     }
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-            redirect(base_url("Bank"));  
+                    echo "<h2>Bank API - UCD CS batch 7 final project_ team exit - https://github.com/murtazasmart/stock-market-bank-service</h2>";
+
 	}
 }
