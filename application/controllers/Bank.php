0abbc3825b54370b10b847713007ebee51344638
lalendra94 <kasun.silva94@yahoo.com>
2018-06-23 13:59:56 +0530

    game id update done

--- application/controllers/Bank.php
+++ application/controllers/Bank.php
@@ -130,8 +130,6 @@ class Bank extends REST_Controller {
                     ], REST_Controller::HTTP_BAD_REQUEST);
         } else {
             $update = $this->AccountModel->updateGameID($oldId, $newId);
-            print_r($update);
-            exit();
             if ($update) {
                 $this->response([
                     'status' => TRUE,
