d19400ab61b6f1c6ecbc70dba943a86f0866f2a1
lalendra94 <kasun.silva94@yahoo.com>
2018-06-23 13:51:39 +0530

    game id update test3

--- application/models/AccountModel.php
+++ application/models/AccountModel.php
@@ -28,11 +28,11 @@ class AccountModel extends CI_Model {
         return count($this->db->query($sql)->result());
     }
 
-    public function validateAccountNumber($accountNumber) { 
+    public function validateAccountNumber($accountNumber) {
         $sql = "SELECT accountNumber FROM `bankaccount` where accountNumber='$accountNumber'";
-        if(count($this->db->query($sql)->result())>0){  
+        if (count($this->db->query($sql)->result()) > 0) {
             return TRUE;
-        }  else {
+        } else {
             return FALSE;
         }
     }
@@ -49,18 +49,23 @@ class AccountModel extends CI_Model {
         $sql = "Delete FROM `bankaccount` where accountNumber='$accountNumber'";
         return $this->db->query($sql);
     }
-    
+
     public function startgame() {
         $this->db->trans_start();
-        $this->db->query("DELETE FROM `bankaccount` WHERE 1"); 
-        $this->db->query("ALTER TABLE bankaccount  AUTO_INCREMENT=10001"); 
+        $this->db->query("DELETE FROM `bankaccount` WHERE 1");
+        $this->db->query("ALTER TABLE bankaccount  AUTO_INCREMENT=10001");
         return $this->db->trans_complete();
     }
-    
-      //updateGameId
-     function updateGameID($oldId,$newId) {
-        $sql="update bankaccount set gameId='$newId' where gameId='$oldId'";
-        return $this->db->query($sql);
+
+    //updateGameId
+    function updateGameID($oldId, $newId) {
+        $sql = "update bankaccount set gameId='$newId' where gameId='$oldId'";
+        $this->db->query($sql);
+        if ($this->db->affected_rows() > 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 
 }
